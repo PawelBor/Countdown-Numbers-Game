31a7303e9c9c26105d91664fa0a5dc982be7039d
Pawel Borzym <pawel.borzym3095@gmail.com>
2017-02-15 11:41:29 +0000

    Select random element from a list of elements

--- numbersGame.rkt
+++ numbersGame.rkt
@@ -4,6 +4,7 @@
 (define numList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 
 ;Generate Random Number between 101 and 999
+;define rand function, call the "random" library function, give it range min max.
 (define rand
   (random 101 1000))
 
@@ -11,3 +12,8 @@
 ;Generate Random number with user input: Min/Max
 (define (inp-rand x y)
  (random x y))
+
+;[1]
+;Select random element from a list.
+(define (rand-element list)
+  (list-ref list (random (length list))))
