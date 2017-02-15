a9759fac9786b8268a94ec527688872ead93fb61
Pawel Borzym <pawel.borzym3095@gmail.com>
2017-02-15 00:14:13 +0000

    Generate Number between x - y | 2 vers

--- numbersGame.rkt
+++ numbersGame.rkt
@@ -2,3 +2,12 @@
 
 ;List of numbers used in Countdown Game
 (define numList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+
+;Generate Random Number between 101 and 999
+(define rand
+  (random 101 1000))
+
+
+;Generate Random number with user input: Min/Max
+(define (inp-rand x y)
+ (random x y))
